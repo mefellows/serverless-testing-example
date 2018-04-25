23f4a40ee60e2041247c93ffab99bf64d0f5b2cd
Matt Fellows <matt.fellows@onegeek.com.au>
2018-04-25 21:13:35 +1000

    wip: add CFN distribution

--- frontend/scripts/deploy.sh
+++ frontend/scripts/deploy.sh
@@ -18,5 +18,5 @@ aws s3 sync build s3://${bucket_name}/ --exclude ".git*" --exclude "*.map" --exc
 aws s3 cp build/index.html s3://${bucket_name}/index.html --cache-control "max-age=30" --acl public-read
 
 step "Done!"
-domain=$(get_stack_output $stack_name WebsiteURL)
+domain=$(get_stack_output $stack_name DomainName)
 log "Head on over to ${domain}"
