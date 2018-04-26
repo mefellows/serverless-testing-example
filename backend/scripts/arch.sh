82a5fbf3f7b96ee2826b675ea77547f060e65a9c
Matt Fellows <matt.fellows@onegeek.com.au>
2018-04-26 14:53:06 +1000

    wip: simplify settings

--- backend/scripts/arch.sh
+++ backend/scripts/arch.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 echo "digraph { ranksep=3; ratio=auto; overlap=false; node [  shape = plaintext, fontname = "Helvetica" ];" > latest.dot ; curl -v -u 'dXfltyFMgNOFZAxr8io9wJ37iUpY42M:O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1' https://test.pact.dius.com.au/pacts/latest | jq '.pacts[]._embedded | select(.consumer.name | contains("AWSSummiteer")) | .consumer.name + "->" + .provider.name' | tr -d '"' |  sed 's/-/_/g' | sed 's/_>/->/g' >> latest.dot; echo "}" >> latest.dot
-dot latest.dot -oarch.png -Tpng
-open arch.png
+file="$(mktemp).png"
+dot latest.dot -o$file -Tpng
+rm latest.dot
+open $file
 
 
